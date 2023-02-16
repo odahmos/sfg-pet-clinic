8e68f99f062417cbf1f4aec538c06f74f216b140
Odai Dahmos <odahmos@atypon.com>
2023-02-16 14:17:02 +0300

    Initial Project add

---
+++ src/main/java/com/example/myptclinic/MyPtclinicApplication.java
@@ -0,0 +1,13 @@
+package com.example.myptclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPtclinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyPtclinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/myptclinic/MyPtclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myptclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPtclinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
